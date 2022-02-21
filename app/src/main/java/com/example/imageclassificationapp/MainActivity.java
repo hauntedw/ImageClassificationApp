3f6bacc88c4716e31792cbb82ce4b1d8ab5bbeec
hauntedw <hindsedward@gmail.com>
2022-02-20 22:14:22 -0500

    Update MainActivity.java
    Updated to add model prediction functionality using resnet18

--- app/src/main/java/com/example/imageclassificationapp/MainActivity.java
+++ app/src/main/java/com/example/imageclassificationapp/MainActivity.java
@@ -1,30 +1,52 @@
 package com.example.imageclassificationapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import org.pytorch.IValue;
+import org.pytorch.LiteModuleLoader;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
-
-    Bitmap bitmap;
     Uri selectedImage;
     ImageView imageView;
+    Bitmap bitmap = null;
+    Module module = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            module = LiteModuleLoader.load(assetFilePath(this, "model.pt"));
+        } catch (IOException e) {
+            Log.e("PytorchTest", "Error reading assets", e);
+            finish();
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
@@ -43,7 +65,26 @@ public class MainActivity extends AppCompatActivity {
         identify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
+                        TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
 
+                final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+
+                final float[] scores = outputTensor.getDataAsFloatArray();
+
+                float maxScore = -Float.MAX_VALUE;
+                int maxScoreIdx = -1;
+                for (int i= 0; i < scores.length; i++){
+                    if (scores[i] > maxScore){
+                        maxScore = scores[i];
+                        maxScoreIdx = i;
+                    }
+                }
+
+                String className = ImageNetClasses.IMAGENET_CLASSES[maxScoreIdx];
+
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(className);
             }
         });
     }
@@ -54,6 +95,30 @@ public class MainActivity extends AppCompatActivity {
             selectedImage = data.getData();
             ImageView imageView = findViewById(R.id.imageView);
             imageView.setImageURI(selectedImage);
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static String assetFilePath(Context context, String assetName) throws IOException {
+        File file = new File(context.getFilesDir(), assetName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
+
+        try (InputStream is = context.getAssets().open(assetName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
         }
     }
 }
